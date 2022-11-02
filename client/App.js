63390cadd1145a74dc273c76f40ad58a56c4d1a1
Saif Maknojia <saifmaknojia@Saifs-MacBook-Pro.local>
2022-11-02 12:20:14 -0400

    project setup and added dependencies to client and server

---
+++ client/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Lets build an invoice app</Text>
+    </View>
+  );
+};
+
+export default App;
