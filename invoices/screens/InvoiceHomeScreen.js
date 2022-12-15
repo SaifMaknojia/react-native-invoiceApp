8f63d288a1ebd8781ae4aa5d531105453b98d537
Saif Maknojia <saif.maknojia@outlook.com>
2022-12-15 14:22:16 -0500

    reorganised the structure

--- invoices/screens/InvoiceHomeScreen.js
+++ invoices/screens/InvoiceHomeScreen.js
@@ -1,15 +1,15 @@
-import {Text, SafeAreaView} from 'react-native';
-import React, {useEffect} from 'react';
+import {Text, SafeAreaView, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import Header from '../components/Header';
 import * as Keychain from 'react-native-keychain';
-import HomeScreenNav from '../components/HomeScreenNav';
-import HomeScreenInvoiceCard from '../components/HomeScreenInvoiceCard';
+import HomeScreenNav from '../components/homeScreen/HomeScreenNav';
+import HomeScreenInvoiceCard from '../components/homeScreen/HomeScreenInvoiceCard';
 import {useDispatch} from 'react-redux';
 import {addInvoice} from '../redux/slice/InvoiceSlice';
 
 const InvoiceHomeScreen = props => {
-  const [token, setToken] = React.useState();
+  const [token, setToken] = useState();
 
   const {navigation} = props;
 
@@ -20,12 +20,12 @@ const InvoiceHomeScreen = props => {
       .get('http://10.0.0.76:8000/api/v1/invoice')
       .then(res => dispatch(addInvoice(res.data)))
       .catch(err => console.log(err));
-  }, []);
+  }, [dispatch]);
 
   const getToken = async () => {
     try {
-      const token = await Keychain.getGenericPassword();
-      setToken(token.password);
+      const newToken = await Keychain.getGenericPassword();
+      setToken(newToken.password);
     } catch (err) {
       console.log(err);
     }
@@ -35,7 +35,7 @@ const InvoiceHomeScreen = props => {
 
   return (
     <>
-      <SafeAreaView style={{backgroundColor: '#fff'}}>
+      <SafeAreaView style={styles.safeAreaView}>
         <Header />
       </SafeAreaView>
       <HomeScreenNav />
@@ -44,5 +44,10 @@ const InvoiceHomeScreen = props => {
     </>
   );
 };
+const styles = StyleSheet.create({
+  safeAreaView: {
+    backgroundColor: '#fff',
+  },
+});
 
 export default InvoiceHomeScreen;
